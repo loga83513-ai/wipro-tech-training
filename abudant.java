0994c9ca168099a324c962a22ba739411672c8b4
loga83513-ai <loga83513@gmail.com>
2026-01-09 10:35:37 +0530

    Create abudant.java

---
+++ abudant.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum=sum+i;
+            }
+        }
+        if(num<sum){
+            System.out.println("Abundant Number");
+        }
+        else{
+            System.out.println("Not Abundant Number");
+        }
+    }
+}
